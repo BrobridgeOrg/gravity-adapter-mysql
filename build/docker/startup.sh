b748a1290bb755d8bea5aff0592ae80f8a3523ef
JheSue <jhe@brobridge.com>
2024-08-06 13:00:31 +0800

    added github action workflow

---
+++ build/docker/startup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+get_args() {
+	_arg=$1
+	shift
+	echo "$@" | grep -Eo "\-\-${_arg}=[^ ]+" | cut -d= -f2- | tail -n 1
+	unset _arg
+}
+
+#sources=$(get_args sources "$@")
+sources=${1#--sources=}
+
+configPath="./settings/sources.json"
+
+[ "$GRAVITY_ADAPTER_MYSQL_SOURCE_CONFIG" != "" ] && {
+	configPath=$GRAVITY_ADAPTER_MYSQL_SOURCE_CONFIG
+} 
+
+[ "$sources" != "" ] && {
+	echo $sources > $configPath
+}
+
+[ "$sources" != "" ] || {
+	echo $GRAVITY_ADAPTER_MYSQL_SOURCE_SETTINGS > $configPath
+}
+
+exec /gravity-adapter-mysql
